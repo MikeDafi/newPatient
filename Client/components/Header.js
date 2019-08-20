8fcf3c98d21dee36d4a425682fcce39f6878c85c
Mike <michael.kaufman@ngc.com>
2019-08-20 11:56:26 -0700

    initial commit

--- Client/components/Header.js
+++ Client/components/Header.js
@@ -4,8 +4,8 @@ const Header = () => (
     <nav className="header navbar navbar-expand-sm navbar-dark bg-react-black">
         <div className="container">
             <div className="navbar-brand">
-                <i className="fa fa-pencil-square-o fa-4x align-middle mr-2 text-react-blue"></i>
-                <span className="align-middle text-react-blue">NoteWorx</span>
+                <i className="fa fa-user-md -o fa-4x align-middle mr-2 text-react-blue"></i>
+                <span className="align-middle text-react-blue">newworldforanewpatient</span>
             </div></div>
     </nav>
 );
