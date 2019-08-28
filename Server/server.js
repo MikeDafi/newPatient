2d3c95d2f379f072d9d9355f5d9812de290166dd
Michael Askndafi <michael.askndafi@ngc.com>
2019-08-28 13:59:41 -0700

    Working Login

--- Server/server.js
+++ Server/server.js
@@ -3,22 +3,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+const passport = require("passport");
+const mongoose = require("mongoose");
+
 
 // app references
 const notesRouter = require('./routers/notes-router');
-
+const users = require("../mern-auth/routes/api/users");
 // initialization
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // configure server
 
 const server = express();
 
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use(cors());
 server.use(morgan('combined'));
 server.use('/api', notesRouter(PORT));
+
+const db = require("../mern-auth/config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+server.use(passport.initialize());
+
+  // Passport config
+  require("../mern-auth/config/passport")(passport);
+  
+  // Routes
+server.use("/api/users", users);
 
 
 // start server
